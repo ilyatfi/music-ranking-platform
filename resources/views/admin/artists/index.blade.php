12d246e179b861de79f8481e595b486bcc5c6847
diliny867 <100518672+diliny867@users.noreply.github.com>
2025-06-17 23:48:53 +0300

    add design to artism moderation page

--- resources/views/admin/artists/index.blade.php
+++ resources/views/admin/artists/index.blade.php
@@ -1,38 +1,55 @@
 <x-layout title="All Artists">
     <div class="container">
-        <h2>{{ __('All Artists') }}</h2>
-        
+
+        <h2 class="mb-4">{{ __('All Artists') }}</h2>
+
         @if (session('success'))
-        <div style="color: green;">{{ session('success') }}</div>
+            <div class="alert alert-success">{{ session('success') }}</div>
         @endif
 
-        <div><a class="btn" href="{{ route('admin.artists.create') }}">{{ __('Add Artist') }}</a></div>
+        {{-- Add Artist Button --}}
+        <div class="mb-3">
+            <a href="{{ route('admin.artists.create') }}" class="btn btn-primary">
+                + {{ __('Add Artist') }}
+            </a>
+        </div>
 
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>{{ __('Stage Name') }}</th>
-                    <th>{{ __('User Name') }}</th>
-                    <th>{{ __('Actions') }}</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($artists as $artist)
+        {{-- Artists Table --}}
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover align-middle shadow-sm">
+                <thead class="table-light">
                     <tr>
-                        <td>{{ $artist->id }}</td>
-                        <td>{{ $artist->stage_name }}</td>
-                        <td>{{ $artist->user->username }}</td>
-                        <td>
-                            <form action="{{ route('admin.artists.destroy', $artist) }}" method="POST" onsubmit="return confirm(`{{ __('Are you sure you want to delete this artist?') }}`)">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit">{{ __('Delete') }}</button>
-                            </form>
-                        </td>
+                        <th scope="col">#</th>
+                        <th scope="col">{{ __('Stage Name') }}</th>
+                        <th scope="col">{{ __('User Name') }}</th>
+                        <th scope="col">{{ __('Actions') }}</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @forelse ($artists as $artist)
+                        <tr>
+                            <td>{{ $artist->id }}</td>
+                            <td>{{ $artist->stage_name }}</td>
+                            <td>{{ $artist->user->username }}</td>
+                            <td>
+                                <form action="{{ route('admin.artists.destroy', $artist) }}"
+                                      method="POST"
+                                      onsubmit="return confirm('{{ __('Are you sure you want to delete this artist?') }}')">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-sm btn-danger">
+                                        {{ __('Delete') }}
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="4" class="text-muted text-center">{{ __('No artists found.') }}</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
     </div>
 </x-layout>
